63b1fc3799f92738e635e0077d955d313cafeaab
kebab000 <dldytpq7010@gmail.com>
2023-07-18 22:06:36 +0900

    car showcase

---
+++ components/ShowMore.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { ShowMoreProps } from "@/types";
+import { updateSearchParams } from "@/utils";
+import { CustomButton } from "@/components";
+
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit" , `${newLimit}`);
+    setLimit(newLimit);
+
+    router.push(newPathName);
+
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
